cc957b601484fe0980382122a215644f62d194a1
David Mair Spiess <david.spiess@hgv.it>
2018-05-04 08:08:59 +0200

    Add simple parser

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { Parser } from "cxml";
+import * as fs from 'fs';
+import * as path from 'path';
+import * as mss from '../xmlns/mss';
+
+const parser = new Parser();
+
+const result = parser.parse(fs.createReadStream(path.resolve(__dirname, 'mss.xml')), mss.document);
+
+result.then((doc: mss.document) => {  
+  console.log(doc.root.header.result_id);
+});
