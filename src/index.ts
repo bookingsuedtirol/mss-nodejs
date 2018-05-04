2dca8d2c51018a8009feeb8ef27c65db6e7836da
David Mair Spiess <david.spiess@hgv.it>
2018-05-04 14:03:44 +0200

    Use prettier

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { Parser } from "cxml";
-import * as fs from 'fs';
-import * as path from 'path';
-import * as getHotelList from '../xmlns/getHotelList';
+import * as fs from "fs";
+import * as path from "path";
+import * as getHotelList from "../xmlns/getHotelList";
 
 const parser = new Parser();
 
-const result = parser.parse(fs.createReadStream(path.resolve(__dirname, 'getHotelList.xml')), getHotelList.document);
+const result = parser.parse(
+  fs.createReadStream(path.resolve(__dirname, "getHotelList.xml")),
+  getHotelList.document
+);
 
 result.then((doc: getHotelList.document) => {
   console.log(doc.root.result);
