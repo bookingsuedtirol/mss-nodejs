609b889a48f3f82e5cda7c446f62e9563679d417
David Mair Spiess <david.spiess@hgv.it>
2018-05-07 09:10:08 +0200

    First working prototype

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
 import { Parser } from "cxml";
-import * as fs from "fs";
-import * as path from "path";
 import * as getHotelList from "../xmlns/getHotelList";
+import fetch from "node-fetch";
 
 const parser = new Parser();
 
-const result = parser.parse(
-  fs.createReadStream(path.resolve(__dirname, "getHotelList.xml")),
-  getHotelList.document
-);
-
-result.then((doc: getHotelList.document) => {
-  console.log(doc.root.result);
-});
+fetch("https://www.easymailing.eu/mss/mss_service.php", {
+  method: "POST",
+  headers: { "Content-Type": "text/xml" },
+  body: `<?xml version="1.0"?>
+  <root>
+    <version>1.0</version>
+    <header>
+      <credentials>
+        <user>${process.env.MSS_USER}</user>
+        <password>${process.env.MSS_PASSWORD}</password>
+        <source>${process.env.MSS_SOURCE}</source>
+      </credentials>
+      <method>getHotelList</method>
+      <paging>
+        <start>0</start>
+        <limit>10</limit>
+      </paging>
+    </header>
+    <request>
+      <search>
+        <lang>de</lang>
+      </search>
+    </request>
+  </root>`
+})
+  .then<getHotelList.document>(res =>
+    parser.parse(res.body, getHotelList.document)
+  )
+  .then(body => console.log(body.root.result.hotel[0]));
