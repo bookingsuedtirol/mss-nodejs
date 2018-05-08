8cf273e9b4a87afb5d0bc8bfb32a76b3390095a6
David Mair Spiess <david.spiess@hgv.it>
2018-05-08 09:07:34 +0200

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -12,31 +12,11 @@ const request: DeepPartial<MssRequest> = {
   },
   request: {
     options: {
-      hotel_details:
-        HotelDetails.BasicInfo |
-        HotelDetails.Address |
-        HotelDetails.Contacts |
-        HotelDetails.Coordinates |
-        HotelDetails.DetailedHotelFacilities |
-        HotelDetails.FullDescription |
-        HotelDetails.Gallery |
-        HotelDetails.GeographicalInformationAsText |
-        HotelDetails.GeographicInformation |
-        HotelDetails.HeaderImages |
-        HotelDetails.HotelFacilities |
-        HotelDetails.HotelMatching |
-        HotelDetails.HotelNavigatorData |
-        HotelDetails.Logo |
-        HotelDetails.LtsSpecificParameters |
-        HotelDetails.PaymentOptionsAtHotel |
-        HotelDetails.PaymentOptionsForOnlineBooking |
-        HotelDetails.SalesPoint |
-        HotelDetails.ShortDescription |
-        HotelDetails.Themes
+      hotel_details: HotelDetails.BasicInfo
     }
   }
 };
 
 mss<getHotelList.document>(request).then(res => {
-  res.root.result.hotel.forEach(hotel => console.log(hotel.pictures));
+  console.log(res.root.result.hotel);
 });
