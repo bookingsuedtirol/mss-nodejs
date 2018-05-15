5a6769c79334166703b88bdd5b103d6885f7fda3
David Mair Spiess <david.spiess@hgv.it>
2018-05-15 16:02:38 +0200

    Cleanup

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var client_1 = require("./client");
-var Request = require("./types/request");
-exports.Request = Request;
-var Response = require("./types/response");
-exports.Response = Response;
-exports.default = client_1.client;
